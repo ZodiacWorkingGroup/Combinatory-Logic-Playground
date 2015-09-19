851a065f6d23db407b78c890dca72779083e95b1
Nathan <nathantronnes@gmail.com>
2015-09-19 13:22:06 -0800

    Working on AST
    This is far from usage-ready.

--- AST.py
+++ AST.py
@@ -3,9 +3,16 @@ class Result:
         self.value = value
         self.combinators = combinators
 
+
 class Expression:  # Credit to Tanner Swett
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+
+    def eval(self, combinators):  # overridden by subclasses
+        pass
+
+    def apply(self, combinators):  # overridden by subclasses
+        pass
 
 
 class CombinatorExpression(Expression):
@@ -23,8 +30,14 @@ class ApplyExpression(Expression):
     def __init__(self, comb, argument):
         self.comb = comb
         self.arg = argument
+        self._arguments = []
 
     def eval(self, combinators):
+        return self.apply(self.arg, combinators)
+
+    def apply(self, arg, combinators):
+        self._arguments.append(arg)
+
         if self.comb == 'S':
             return Result(None, combinators)
         elif self.comb == 'K':
--- lexer.py
+++ lexer.py
@@ -2,16 +2,23 @@ from UtopiaLexer import *
 
 COMBINATOR = 'COMB'
 PAREN = 'PAREN'
+RESERVED = 'RES'
 
 
-def lexscript(script):
+def lexline(line):
     lex = lexer()
 
     lex.add_token_expr(r"^[\s]", None)
+    lex.add_token_expr(r"import", RESERVED)
+    lex.add_token_expr(r"=", RESERVED)
     lex.add_token_expr(r"[()]", PAREN)
     lex.add_token_expr(r"[A-Z][a-z0-9_*]*", COMBINATOR)
 
-    return lex.lex(script)
+    return lex.lex(line)
+
+
+def lexscript(script):
+    return [lexline(x) for x in script.split('\n')]
 
 if __name__ == '__main__':
     print(lexscript(input()))
